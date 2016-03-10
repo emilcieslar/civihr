9f3d05b078835ca31d29b11109e6b115ec80c64e
Emil Cieslar <cieslar@webkreativ.cz>
2016-03-10 12:24:13 +0100

    Roadmap timeline in progress

--- README.md
+++ README.md
@@ -35,3 +35,5 @@ This will compile all js and scss.
 1. You may have noticed that top-bar is absolute positioned for screen sizes large and up. I could simply place the header inside .homepage-intro section and therefore making it static. However as other parts of the site (such as Blog) don't have the .homepage-intro section with transparent top-bar over an image, programmer would not be able to reuse the top-bar into header section in a generic layout. This way a programmer can easily remove .absolute and/or .transparent classes from .top-bar-wrapper making the top-bar static and usable on other parts such as Blog. Also adding .no-border class to the .top-bar would remove the white bottom border.
 
 2. Typekit fonts won't work on different domain than specified so please make sure you're viewing the site on [provided page](http://emilcieslar.github.io/civihr).
+
+3. Some parts are not refined to pixel perfect details, which is due to time constraints. My strategy was to show as much as possible to the reasonable detail.
--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
 $(document).foundation();
+
+$(document).ready(function() {
+
+  // ----------------------------------------------------------------------------> SET THE WIDTH OF ROADMAP
+  var roadMapWidth = 0;
+  $('.roadmap-inner .timeline-up-down').each(function() {
+    roadMapWidth += $(this).outerWidth(true);
+  });
+  $('.roadmap-inner').width(roadMapWidth/2);
+
+  // ----------------------------------------------------------------------------> SET HEIGHT OF ROADMAP WRAPPER
+  $('.roadmap-wrapper').height($('.roadmap-inner').outerHeight(true));
+
+  // ----------------------------------------------------------------------------> LOAD TIMELINE DARK LINE BG
+  $('.dark-line').addClass('full');
+
+  // ----------------------------------------------------------------------------> MOVING ROADMAP'S TIMELINE
+  $('.roadmap-arrow.right').click(function() {
+
+    // Display left arrow if not visible
+    var leftArr = $('.roadmap-arrow.left');
+    if(!leftArr.is(":visible")) {
+
+      // Calculate how much should be left arrow moved
+      leftArrW = -leftArr.width() / 2;
+      // Fade In the left arrow
+      leftArr.fadeIn();
+
+      // Animate the movement
+      leftArr.animate({
+        left: leftArrW + "px"
+      },1000);
+
+    }
+
+    // Calculate the size that the roadmap-inner should be moved
+    var roadmapWrapperWidth = $('.roadmap-wrapper').outerWidth(true)
+    var moveBy = roadmapWrapperWidth < $('.roadmap-inner').width() - roadmapWrapperWidth ? roadmapWrapperWidth : $('.roadmap-inner').width() - roadmapWrapperWidth;
+
+    $('.roadmap-inner').animate({
+      left: "-=" + $('.roadmap-wrapper').outerWidth(true)
+    },1000);
+  });
+
+
+});
